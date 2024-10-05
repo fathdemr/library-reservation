15906e1336445366f2c452c7e9fcad6caacc5610
fatihdemir <fath.demmr@gmail.com>
2024-10-05 18:38:13 +0300

    Initial commit

---
+++ src/test/java/com/fatihdemir/libraryreservation/LibraryReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatihdemir.libraryreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
