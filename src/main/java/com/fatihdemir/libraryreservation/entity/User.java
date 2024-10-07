1bebea6e92e030506cf5e51a602f69d6154d2acb
fatihdemir <fath.demmr@gmail.com>
2024-10-07 23:56:39 +0300

    JWT and Security added

--- src/main/java/com/fatihdemir/libraryreservation/entity/User.java
+++ src/main/java/com/fatihdemir/libraryreservation/entity/User.java
@@ -3,28 +3,41 @@ package com.fatihdemir.libraryreservation.entity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serializable;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+@Builder
+public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
-    private String name;
+    private String firstname;
+
+    private String lastname;
 
     private String email;
+
+    private String password;
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private Date birtOfDate;
@@ -39,7 +52,7 @@ public class User {
 
     private Long phoneNum;
 
-    private String hash; // Password hash of the user MD5
+    private String hash; // Password hash of the  MD5
 
     @CreationTimestamp
     private Date createdAt;
@@ -47,4 +60,41 @@ public class User {
     @UpdateTimestamp
     private Date updatedAt;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return hash;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
